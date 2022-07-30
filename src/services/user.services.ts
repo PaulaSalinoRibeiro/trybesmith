43a0fa84668b5dbcb2dec5698e1d07084a835b03
Paula <paulasalinor@gmail.com>
2022-07-30 17:35:14 -0300

    :sparkles: add user validated
    add validated for username, classe, level and password

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,8 +1,37 @@
+import joi from 'joi';
+import { throwError } from '../helpers/throwError';
 import * as UserModel from '../models/user.model';
 import { createToken } from '../helpers/jwt';
 import IUser from '../interfaces/IUser';
 
+const keysValidated = (data: IUser): void => {
+  const schema = joi.object().keys({
+    username: joi.required(),
+    classe: joi.required(),
+    level: joi.required(),
+    password: joi.required(),
+  });
+
+  const { error } = schema.validate(data);
+  if (error) throwError('BadRequest', error.message);
+};
+
+const valuesValidate = (data: IUser): void => {
+  const schema = joi.object({
+    username: joi.string().min(3).required(),
+    classe: joi.string().min(3).required(),
+    level: joi.number().min(1).required(),
+    password: joi.string().min(8).required(),
+  });
+
+  const { error } = schema.validate(data);
+  if (error) throwError('Unprocessable', error.message);
+};
+
 export const createUser = async (user: IUser): Promise<string> => {
+  keysValidated(user);
+  valuesValidate(user);
+
   await UserModel.createUser(user);
   const token = createToken(user.username);
   return token;
